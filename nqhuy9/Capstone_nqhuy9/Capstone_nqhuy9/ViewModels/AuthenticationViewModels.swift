7c42e3cf5c9d3c7239b7680e40757f18a17d176c
Nghiêm Huy <huynghiem99@gmail.com>
2021-07-08 15:53:01 +0700

    Init authenticationViewModel

---
+++ nqhuy9/Capstone_nqhuy9/Capstone_nqhuy9/ViewModels/AuthenticationViewModels.swift
@@ -0,0 +1,33 @@
+//
+//  AuthenticationViewModels.swift
+//  Capstone_nqhuy9
+//
+//  Created by Nghiêm Huy on 7/8/21.
+//
+
+import Foundation
+
+protocol AuthenticationViewModels {
+    func signIn(account: Account) -> Bool
+    
+    func signUp(account: Account)
+}
+
+struct DefaultAuthenticationViewModel: AuthenticationViewModels {
+    
+    var repository : DefaultAuthenticationRepository
+    
+    init() {
+        self.repository = DefaultAuthenticationRepository()
+    }
+    
+    func signIn(account: Account) -> Bool {
+        return repository.signIn(account: account)
+    }
+    
+    func signUp(account: Account) {
+        repository.signUp(account: account)
+    }
+    
+    
+}
