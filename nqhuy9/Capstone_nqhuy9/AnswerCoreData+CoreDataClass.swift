e13635aa1bb2fbeadaacf5d69908f2ce96f9e044
Nghiêm Huy <huynghiem99@gmail.com>
2021-07-12 16:07:25 +0700

    Generate AnswerCoreData Model

---
+++ nqhuy9/Capstone_nqhuy9/AnswerCoreData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  AnswerCoreData+CoreDataClass.swift
+//  Capstone_nqhuy9
+//
+//  Created by Nghiêm Huy on 7/12/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class AnswerCoreData: NSManagedObject {
+
+}
