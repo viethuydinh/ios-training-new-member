d06c7237888d743a20a57744aae47ab2dfe06741
Nghiêm Huy <huynghiem99@gmail.com>
2021-07-22 15:52:47 +0700

    Add custom AlertView

---
+++ nqhuy9/Capstone_nqhuy9/Capstone_nqhuy9/Views/CustomAlert/AlertCustomView.swift
@@ -0,0 +1,20 @@
+//
+//  AlertCustomView.swift
+//  Capstone_nqhuy9
+//
+//  Created by Nghiêm Huy on 22/07/2021.
+//
+
+import UIKit
+
+class AlertCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
