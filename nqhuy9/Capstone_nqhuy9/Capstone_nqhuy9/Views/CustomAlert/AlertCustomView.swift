661a6f2e0bd21de62367be49bfb0b0b052f23489
Nghiêm Huy <huynghiem99@gmail.com>
2021-07-22 15:55:55 +0700

    Add Custom Alert

--- nqhuy9/Capstone_nqhuy9/Capstone_nqhuy9/Views/CustomAlert/AlertCustomView.swift
+++ nqhuy9/Capstone_nqhuy9/Capstone_nqhuy9/Views/CustomAlert/AlertCustomView.swift
@@ -7,14 +7,66 @@
 
 import UIKit
 
-class AlertCustomView: UIView {
+class AlertCustomView: UIView, NibLoadableView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var contentView: UIView!
+    
+    private var callback:(()->Void)?
+    
+    convenience init(title: String,content: String,isHiddenCancel: Bool = false,callback:(()->Void)?){
+        self.init()
+        self.titleLabel.text = title
+        self.contentLabel.text = content
+        self.cancelButton.isHidden = isHiddenCancel
+        self.callback = callback
     }
-    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+    func setup() {
+        connectNibUI()
+        self.setupButton()
+        self.contentView.corner(radius: 10.0)
+    }
+    
+    func setupButton() {
+        self.okButton.corner(radius: 4.0)
+        self.okButton.setTitleColor(.white, for: .normal)
+        self.cancelButton.layer.borderWidth = 1.0
+        self.cancelButton.layer.borderColor = UIColor.frenchBlue.cgColor
+        self.cancelButton.corner(radius: 4.0)
+    }
+    
+    func show(superView: UIView) {
+        self.showWithAnimation(animatedView: contentView, in: superView)
+    }
+    
+    func hide() {
+        self.hideWithAnimation(animatedView: contentView, callback: callback)
+    }
+    
+    func hideCancel() {
+        self.hideWithAnimation(animatedView: contentView, callback: nil)
+    }
+    
+    @IBAction func onClickOk(_ sender: Any) {
+        self.hide()
+    }
+    
+    @IBAction func onClickCancel(_ sender: Any) {
+        self.hideCancel()
+    }
 
 }
