4a7e18a3882fdb841e141cb2a6ef4ca55ad2d3d8
Nghiêm Huy <>
2021-08-19 16:26:04 +0700

    Fix return bool in firebase

--- nqhuy9/Capstone_nqhuy9/Capstone_nqhuy9/Database/FirebaseDatabase/BaseFirebase.swift
+++ nqhuy9/Capstone_nqhuy9/Capstone_nqhuy9/Database/FirebaseDatabase/BaseFirebase.swift
@@ -19,13 +19,15 @@ struct FirebaseRepository<Domain: Codable> {
         
     }
     
-    func fetchAll(tableName: String, completion: @escaping(([Domain]) -> ())) {
+    func fetchAll(tableName: String, completion: @escaping(([Domain]) -> ())) -> Bool {
+        var error: Int = 0
         var result: [Domain] = []
         let dispatch = DispatchGroup()
         dispatch.enter()
         db.collection(tableName).getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
+                error += 1
             } else {
                 for document in querySnapshot!.documents {
                     let decoder = JSONDecoder()
@@ -42,14 +44,22 @@ struct FirebaseRepository<Domain: Codable> {
         dispatch.notify(queue: .main) {
             completion(result)
         }
+        
+        if error != 0 {
+            return false
+        } else {
+            return true
+        }
     }
     
-    func save(tableName:String, domain: Domain,id: String, conditionUpdate: [String:Any]) {
+    func save(tableName:String, domain: Domain,id: String, conditionUpdate: [String:Any]) -> Bool {
+        var error: Int = 0
         var ref: DocumentReference? = nil
-        guard let dict = domain.dictionary else { return }
+        guard let dict = domain.dictionary else { return false }
         db.collection(tableName).document(id).getDocument { querySnapshot, err in
             if let err = err {
                 print("Error adding document: \(err)")
+                error += 1
             } else if querySnapshot == nil {
                 print("Create")
                 ref = db.collection(tableName).addDocument(data: dict) { err in
@@ -65,21 +75,44 @@ struct FirebaseRepository<Domain: Codable> {
                 document.reference.updateData(conditionUpdate)
             }
         }
-    }
-    
-    func saveAll(tableName: String, domains: [Domain]) {
-        domains.forEach { domain in
-            self.save(tableName: tableName, domain: domain, id: "", conditionUpdate: [:])
+        
+        if error != 0 {
+            return false
+        } else {
+            return true
         }
     }
     
-    func delete(tableName: String,value: String) {
+    func saveAll(tableName: String, domains: [Domain]) -> Bool {
+        var error: Int = 0
+        domains.forEach { domain in
+            if !self.save(tableName: tableName, domain: domain, id: "", conditionUpdate: [:]) {
+                error += 1
+            }
+        }
+        
+        if error != 0 {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func delete(tableName: String,value: String) -> Bool {
+        var error: Int = 0
         db.collection(tableName).document(value).delete { err in
             if let err = err {
                 print("Error adding document: \(err)")
+                error += 1
             } else {
                 print("Document delete successfully")
             }
+        }
+        
+        if error != 0 {
+            return false
+        } else {
+            return true
         }
     }
 }
