9cca1786f49cf51a992c4c658e92b5f14bfe7f78
Lomzom <bach287@gmail.com>
2022-07-11 22:15:09 +0700

    week 2 push

---
+++ Bachnv/week 2/AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Bách Nguyễn Viết on 10/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
