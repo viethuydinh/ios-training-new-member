a318823c5351ce006328aee7568f5b0caf02c10c
Lomzom <bach287@gmail.com>
2022-07-13 01:35:12 +0700

    apple pie part 1

---
+++ Bachnv/week 2/Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Bách Nguyễn Viết on 13/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
